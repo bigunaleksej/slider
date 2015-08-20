60ace5810488657f960f501b72cd52ad06f5fc4c
bigunaleksej <bigunaleksej@gmail.com>
2015-08-21 01:09:51 +0300

    added images

--- helpers.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by abigun on 8/20/2015.
- */
-function qs(selector) {
-    return window.document.querySelector(selector);
-}
--- slider.js
+++ slider.js
@@ -1,63 +1,43 @@
 /**
  * Created by abigun on 8/20/2015.
  */
-//(function () {
-    var config = {
-    // arbitrary number of images
-    images: [
-        'http://example.com/image-1.jpg',
-        'http://example.com/image-2.jpg',
-        'http://example.com/image-3.jpg',
-        'http://example.com/image-4.jpg'
-    ],
-    // possible values: 'auto', 'manual', 'automanual'
-    mode: 'auto',
-    // arbitrary interger (miliseconds)
-    swipeSpeed: 500,
-    // arbitrary interger (miliseconds). This is used in 'auto' and 'automanual' modes
-    swipeDelay: 3000
-    };
+var Slider = {
+        init: function (id, config) {
+            this._crateSlider(id, config.images);
+            this.currentSlide = 0;
+        },
+        _crateSlider: function (id, images) {
+            var slider = document.createElement('div'),
+                mask = document.createElement('div'),
+                ul = document.createElement('ul'),
+                slide;
 
-    var Slider = {}
+            this._slides = ul.children;
+            slider.setAttribute('class', 'slider');
+            mask.setAttribute('class', 'mask');
 
-    Slider.init = function (id, config) {
-        this.currentSlide = 0;
-        this._crateSlider(id, config.images);
-    };
-
-    Slider._crateSlider = function (id, images) {
-        var slider = document.createElement('div'),
-            mask = document.createElement('div'),
-            ul = document.createElement('ul'),
-            slide, slideState;
-
-        slider.setAttribute('class', 'slider');
-        mask.setAttribute('class', 'mask');
-
-        for (var i = 0, length = images.length; i < length; i++) {
-            slideState = 'inactive';
-            if (i === 0) {
-                slideState = 'current';
-            }
-            if (i === 1) {
-                slideState = 'next';
-            }
-            if (i === length-1) {
-                slideState = 'previous';
+            for (var i = 0, length = images.length; i < length; i++) {
+                slide = document.createElement('li');
+                slide.id = 'slide' + i;
+                slideImg = document.createElement('img');
+                slideImg.src = images[i];
+                slide.appendChild(slideImg);
+                ul.appendChild(slide);
             }
 
-            slide = document.createElement('li');
-            slide.setAttribute('class', slideState);
-            slide.textContent = images[i];
-            ul.appendChild(slide)
+            this._slides[0].setAttribute('class', 'current');
+            this._slides[1].setAttribute('class', 'next');
+            this._slides[this._slides.length-1].setAttribute('class', 'previous');
+
+            mask.appendChild(ul);
+            slider.appendChild(mask);
+            document.querySelector('#' + id).appendChild(slider);
+        },
+        _next: function () {
+
+        },
+        _previous: function () {
         }
+};
 
-        mask.appendChild(ul);
-        slider.appendChild(mask);
-        qs('#' + id).appendChild(slider);
-    }
 
-    var slider = Object.create(Slider);
-
-    slider.init('slider', config);
-//})();

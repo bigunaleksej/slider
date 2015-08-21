6431312d64b053818299877434c915b46e3a2e99
AlexeyBigun <abigun@softserveinc.com>
2015-08-21 15:06:52 +0300

    Implement logic previous/next change classes

--- slider.js
+++ slider.js
@@ -4,8 +4,10 @@
 var Slider = {
         init: function (id, config) {
             this._crateSlider(id, config.images);
+            this.sliderId = id;
             this.currentSlide = 0;
         },
+
         _crateSlider: function (id, images) {
             var slider = document.createElement('div'),
                 mask = document.createElement('div'),
@@ -18,6 +20,7 @@ var Slider = {
 
             for (var i = 0, length = images.length; i < length; i++) {
                 slide = document.createElement('li');
+                slide.setAttribute('class', 'inactive');
                 slide.id = 'slide' + i;
                 slideImg = document.createElement('img');
                 slideImg.src = images[i];
@@ -33,11 +36,35 @@ var Slider = {
             slider.appendChild(mask);
             document.querySelector('#' + id).appendChild(slider);
         },
-        _next: function () {
 
+        _next: function () {
+            var involvedElements = this._getInvolvedElement(),
+                newInvolvedElement = involvedElements[2].nextElementSibling || this._slides[0];
+
+            involvedElements[0].setAttribute('class', 'inactive');
+            involvedElements[1].setAttribute('class', 'previous');
+            involvedElements[2].setAttribute('class', 'current');
+            newInvolvedElement.setAttribute('class', 'next');
         },
+
         _previous: function () {
-        }
+            var involvedElements = this._getInvolvedElement(),
+                newInvolvedElement = involvedElements[0].previousElementSibling || this._slides[this._slides.length-1];
+
+            involvedElements[2].setAttribute('class', 'inactive');
+            newInvolvedElement.setAttribute('class', 'previous');
+            involvedElements[0].setAttribute('class', 'current');
+            involvedElements[1].setAttribute('class', 'next');
+        },
+
+        _getInvolvedElement: function () {
+            var elments = [];
+
+            elments.push(document.querySelector('#' + this.sliderId + ' .previous'));
+            elments.push(document.querySelector('#' + this.sliderId + ' .current'));
+            elments.push(document.querySelector('#' + this.sliderId + ' .next'));
+            return elments;
+        },
 };
 
 
